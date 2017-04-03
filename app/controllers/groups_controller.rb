2fadbc253fa951d31e9c9f3cfcf4f9aa8e71a8c6
tongtong23 <tongguo89@gmail.com>
2017-04-03 20:12:32 +0800

    add posts nested route

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only:[:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
@@ -31,15 +31,15 @@ class GroupsController < ApplicationController
   end
   end
   def destroy
-    
+
     @group.destroy
     redirect_to groups_path, alert: "Group deleted"
-    redirect_to groups_path
+
   end
   private
   def find_group_and_check_permission
-    @group =Group.find(params[:id])
-    if current_user != @group.user
+    @group = Group.find(params[:id])
+    if current_user !=@group.user
       redirect_to root_path, alert: "You have no permission."
     end
   end
